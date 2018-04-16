df876ab75892e326a7fb9df56991a1c4823a9853
yjwang <yjwang@leyantech.com>
2018-04-16 23:54:02 +0800

    语料中搜索正则

---
+++ re.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import re
+
+txtFile = sys.argv[1]
+regFile = sys.argv[2]
+
+with open(txtFile, 'r') as f1:
+    txtlines = f1.readlines()
+
+with open(regFile, 'r') as f2:
+    regLines = f2.readlines()
+    
+for regLine in regLines:
+    tp_ls = regLine.split('\t')
+    p = re.compile(tp_ls[1])
+    tg_lines = list()
+    for txtline in txtlines:
+        if p.search(txtline):
+            tg_lines.append(txtline)
+    with open(tp_ls[0]+'.txt', 'w') as w3:
+        w3.writelines(tg_lines)
+    
